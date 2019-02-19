f8c966468a8e88d627855702322536606b9f4349
Nissan Holzer <nissan.holzer@gmail.com>
2019-02-19 01:20:32 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
   
   @@all = [] 
   
-  def initialize(date,doctor, patient, date)
+  def initialize(date, doctor, patient)
     @patient = patient 
     @doctor = doctor
     @date = "#{date}"
